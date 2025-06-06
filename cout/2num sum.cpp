440cf4548e6d288896f4dc5dade51bf1e0338fb9
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-06 23:38:20 +0530

    Practic c++

---
+++ cout/2num sum.cpp
@@ -0,0 +1,10 @@
+/* Write a Program to 2 Number sum */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 35, b = 50, sum;
+    sum = a + b;
+    cout<< "Sum = %d"<< sum;
+    return 0;
+}
