f3dfd2bd4a1d499bcb014199cb2c220a2717428e
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-08 17:53:18 +0530

    Practic c++

--- cin/Volume of box.cpp
+++ cin/Volume of box.cpp
@@ -1,2 +1,13 @@
 /* Write a Program to Calculate valume of box */
- 
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+     int l, h, w;
+     float volume_box;
+     cout<<"Enter the lenght,height and wedth : ";
+     cin>>l>>h>>w;
+     volume_box  = l * h * w;
+     cout<<"Volume of Box = "<<volume_box;
+     return 0;
+ }
