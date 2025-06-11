b7cd81dea97ae2bfbec328b1492011f0a4724532
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-11 22:10:43 +0530

    Practic C++

--- Global & Block Veriabal/Global Variable.cpp
+++ Global & Block Veriabal/Global Variable.cpp
@@ -1,11 +1,11 @@
 /* Write a Program to using global Variable */
 #include<iostream>
 using namespace std;
-int g ;
+int g ;         // Global Variable Inisilize
 int main()
 {
     int a = 20, b = 10;
-    g = a + b;
+    g = a + b;          // Global Variable Access
     cout<<"Global Variable Sum = "<<g;
     return 0;
 }
