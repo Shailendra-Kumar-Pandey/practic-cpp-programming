ef4427f92dce398ed5af421da9d0f228180d7759
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-11 22:22:35 +0530

    Practic C++

--- Global & Block Veriabal/Local-Gloabal Variable .cpp
+++ Global & Block Veriabal/Local-Gloabal Variable .cpp
@@ -2,8 +2,10 @@
 #include<iostream>
 using namespace std;
 int g = 20 ;         // Global Variable Declaration and initialization
+int main()
 {
     int g = 10;   //Local Variable Declaration and initialization
-    cout<<g;
+    cout<<"G = "<<g;
     return 0;
 }
+/* If Local & global Variable same name then local variable get 1st Prefrence */
