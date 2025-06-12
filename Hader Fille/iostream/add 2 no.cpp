7631908763551e4a9a66ae98ea8fe2170433de5e
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-12 22:33:00 +0530

    Practic c++

---
+++ Hader Fille/iostream/add 2 no.cpp
@@ -0,0 +1,12 @@
+/* Write a Program to add two Numbers */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, add;
+    cout<<"Enter the Two Numbers : ";
+    cin>>a>>b;
+    add = a + b;
+    cout<<"Sum = "<<add;
+    return 0;
+}
