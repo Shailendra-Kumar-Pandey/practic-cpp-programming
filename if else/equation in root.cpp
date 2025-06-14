2af14a179b3abb9d914bd613e08c9b485f4e8009
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-14 13:59:29 +0530

    practic c++

---
+++ if else/equation in root.cpp
@@ -0,0 +1,17 @@
+/* write a program to calculate and print Roots of equation ax^2+bx+c */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, x;
+    cout<<"Enter the Value a, b and c : ";
+    cin>>a>>b>>c;
+    x = a*a + 4*b*c;
+    if(x = 0)
+        cout<<"good";
+    if(x < 0)
+        cout<<"better";
+    if(x > 0)
+        cout<<"best";
+    return 0;
+}
