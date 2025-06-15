bce0beef547e0366ca197671cf2bb8e96ccc7786
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-15 22:41:30 +0530

    Practic c++

---
+++ Array/marite list 10 student.cpp
@@ -0,0 +1,7 @@
+/* Write a Program to print merit list consider 10 student */
+#include<iostream>
+using  namespace std;
+int main()
+{
+    int 
+}
---
+++ Array/shorting.cpp
@@ -0,0 +1,8 @@
+/* Write a Program to create an array of size 15 load it with any number finaly print number of even element number,
+ Number of odd element, biggest element array as will as smallest element array. */
+ #include<iosteram>
+ using namespace std;
+ int main()
+ {
+    
+ }
