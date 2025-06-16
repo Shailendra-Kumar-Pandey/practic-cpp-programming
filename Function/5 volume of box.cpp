5d44a1302789d7aa8ed6a7a29caa2942c5c86dd2
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-16 23:06:15 +0530

    Practic c++

---
+++ Function/5 volume of box.cpp
@@ -0,0 +1,10 @@
+/* Write a Program to calculate and print Volume of 5 box */
+#include<iostream>
+using namespace std;
+int volume_box(int, int, int);
+int main()
+{
+    int w, l, h, volume;
+    
+    return 0;
+}
