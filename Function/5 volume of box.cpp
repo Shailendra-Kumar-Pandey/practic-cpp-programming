0fdc7baeb68a22b5cc3da0588c4a9c00aeeba35a
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-16 23:20:31 +0530

    Practic c++

--- Function/5 volume of box.cpp
+++ Function/5 volume of box.cpp
@@ -4,7 +4,20 @@ using namespace std;
 int volume_box(int, int, int);
 int main()
 {
-    int w, l, h, volume;
-    
+    int i =0, w, l, h, volume;
+    while(i < 5)
+    {
+        cout<<"Enter the box width, length and height : ";
+        cin>>w>>l>>h;
+        volume = volume_box(w, l, h);
+        cout<<"Volume of Box = "<<volume<<endl;
+        i = i + 1;
+    }
     return 0;
+}
+int volume_box(int x, int y, int z )
+{
+    int box;
+    box = x * y * z;
+    return box;
 }
