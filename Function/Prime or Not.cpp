c7ea846839ceb74a0c71a10278bd9dcfed33f2b3
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-17 20:28:41 +0530

    Practic c++

--- Function/Prime or Not.cpp
+++ Function/Prime or Not.cpp
@@ -1,29 +1,29 @@
-/* Write a program to chack wether given Number Prime or Not */
+/* Write a program to check weather given Number Prime or Not */
 #include<iostream>
 using namespace std;
 int prime_or_not(int);
 int main()
 {
-    int n, result;
+    int n, result=0;
     cout<<"Enter any Number finding a Prime Number or Not : ";
     cin>>n;
-    if(n <=1 )
+    if(n <= 1 )
         cout<<n<<" is not a Prime Number";
     result = prime_or_not(n);
-    cout<<result;
     if(result == 2)
         cout<<n<<" Number is a Prime Number";
     else
-        cout<<n<<" Number is Not a Prime Number";
+        if(n>1)
+            cout<<n<<" Number is Not a Prime Number";
     return 0;
 }
 int prime_or_not(int x)
 {
     int i , count = 0;
-    for(i=1; i<x; i++)
+    for(i=1; i<=x; i++)
     {
         if(x%i==0)
-            count++;
+            count= count+1;
     }
     return count;
 }
