27cc3510d927842f762d70b007f6d27a7e1f0f2a
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-17 21:14:50 +0530

    Practic c++

---
+++ loops/For Loop/1st 20 number prime with chack 0 and 1 this program.cpp
@@ -0,0 +1,23 @@
+/* Write a program to calculate and print 1st 20 prime number and 0 to 1 check prime or not this program*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, c =0;
+    for (i=0; c<20; i++)
+    {
+        if(i<=1)
+            continue;           //Jump the next command
+        for(j=2; j<i; j++)
+        {
+            if(i%j==0)
+                break;
+        }
+        if(i==j)
+        {
+            cout<<" "<<i;
+            c++;
+        }
+    }
+    return 0;
+}
