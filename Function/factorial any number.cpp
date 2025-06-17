b6810e5cc5899302abc3df663eabe9a499af484f
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-17 19:32:09 +0530

    Practic c++

--- Function/factorial any number.cpp
+++ Function/factorial any number.cpp
@@ -1,8 +1,20 @@
 /* Write a Program to calculate and print factorial any number */
 #include<iostream>
 using namespace std;
-factorial
+float factorial(float);
 int main()
 {
-
+    float n, fact;
+    cout<<"Please enter find the number factorial : ";
+    cin>>n;
+    fact = factorial(n);
+    cout<<n<<" is Factorial "<<fact;
+    return 0;
+}
+float factorial(float x)
+{
+    int f = 1,
+    while(x>0)
+        f = f * x--;
+    return f;    
 }
