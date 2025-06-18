ad621965a89647afcca1873477d1814f6bc0aa87
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-18 13:35:58 +0530

    Practic c++

---
+++ Classes And Object/Class Box and object box.cpp
@@ -0,0 +1,28 @@
+/* Write a Program to calculate and print volume of box, create Box and box oject  */
+#include<iostream>
+using namespace std;
+class Box           //Create Class and decliration, initilization 
+{   public:
+        double length;
+        double breadth;
+        double height;
+};
+int main()
+{
+    Box box1;       // Object Create
+    Box box2;
+    double volume_box = 0.0;
+    // Object initilizetion
+    box1.length = 5.0;
+    box1.breadth = 6.0;
+    box1.height = 7.0;
+    // Object initilizetion
+    box2.length = 11.0;
+    box2.breadth = 16.0;
+    box2.height = 17.0;
+    volume_box = box1.length * box1.breadth * box1.height;
+    cout<<"\nVolume of Box-1 = "<<volume_box<<endl;
+    volume_box = box2.length * box2.breadth * box2.height;
+    cout<<"\nVolume of Box-2 = "<<volume_box<<endl;
+    return 0;
+}
