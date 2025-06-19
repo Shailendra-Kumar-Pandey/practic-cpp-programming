06ccd93620df5974b0daac4f71900d34ecbb3d68
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-19 16:13:35 +0530

    Practic c++

--- Classes And Object/Class-member function/Class Ractrangal and object - member function.cpp
+++ Classes And Object/Class-member function/Class Ractrangal and object - member function.cpp
@@ -1,1 +1,47 @@
-/* Write a Program to calculate and print area and Perimeter create Rectrangal in class. */
+/* Write a Program to calculate and print area and Perimeter create Rectangle in class. */
+#include<iostream>
+using namespace std;
+class Rectangale
+{   public:
+        int length;
+        int width;
+        void getarea();
+        void getperimeter();
+        int setlength(int l);
+        int setwidth(int w);
+};
+void Rectangale::getarea()
+{
+    return length * width;
+}
+void Rectangale::getperimeter()
+{
+    return 2 * (length + width);
+}
+int Rectangale::setlength(int l)
+{
+    length = l;
+}
+int Rectangale::setwidth(int w)
+{
+    width = w;
+}
+
+int main()
+{
+    Rectangale r1;
+    Rectangale r2;
+    int area = 0, perimeter = 0;
+    r1.setlength(120);
+    r1.setwidth(80);
+    r2.setlength(100);
+    r2.setwidth(60);
+    area = r1.getarea();
+    perimeter = r1.getperimeter();
+    cout<<"Area of Rectangale R-1 = "<<area<<endl;
+    cout<<"Perimeter of Rectangale R-1 = "<<perimeter<<endl;
+    cout<<"Area of Rectangale R-2 = "<<r2.getarea()<<endl;
+    cout<<"Perimeter of Rectangale R-2 = "<<r2.getperimeter()<<endl;
+    return 0;
+}
+
