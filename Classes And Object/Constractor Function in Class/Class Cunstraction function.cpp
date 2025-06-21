aa7d7413b02653f03fcffc75c55d8fe3efb467af
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-21 21:30:32 +0530

    Practic c++

--- Classes And Object/Constractor Function in Class/Class Cunstraction function.cpp
+++ Classes And Object/Constractor Function in Class/Class Cunstraction function.cpp
@@ -1,1 +1,27 @@
 /* Write to Program to print and create constractor function */
+#include<iostream>
+using namespace std;
+class Line
+{
+    double length;
+    public:
+        void setLength(double l){
+            length = l;
+        }
+        double getLength(){
+            return length;
+        }
+        Line();
+};
+Line::Line(){
+    length = 5.0;
+    cont<<"\n Create an Object automatic call Function...";
+}
+int main()
+{
+    Line l1;
+    cout<<"\n Length = "<<l1.getLength()<<endl;
+    l1.setLength(10.0);
+    cout<<"\n Length = "<<l1.getLength()<<endl;
+    return 0;
+}
