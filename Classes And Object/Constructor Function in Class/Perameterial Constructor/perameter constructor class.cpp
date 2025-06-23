a90a641c3e9d183e82fc6d657a7a9e1396ebfe72
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-23 11:50:00 +0530

    practic c++

--- Classes And Object/Constructor Function in Class/Perameterial Constructor/perameter constructor class.cpp
+++ Classes And Object/Constructor Function in Class/Perameterial Constructor/perameter constructor class.cpp
@@ -20,3 +20,10 @@ Line::Line(double len) {
         cout << "Object created with length: " << length << endl;
         length = len;
     }
+int main() {
+    Line line(10.0); // Create an object of Line with length 10.
+    cout << "Length of line: " << line.getLength() << endl; // Output the length
+    line.setLength(20.0); // Set a new length
+    cout << "New length of line: " << line.getLength() << endl; // Output the new length
+    return 0;
+}
