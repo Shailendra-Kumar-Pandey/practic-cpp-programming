4a17b9be05ded5bc8fbbee9f880fb8749aaf7f2b
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-24 12:53:13 +0530

    Practic c++

--- Classes And Object/Destructor Function in Class/Destructor Function in class.cpp
+++ Classes And Object/Destructor Function in Class/Destructor Function in class.cpp
@@ -1,1 +1,33 @@
 /* Write a Program to calculate and Print - Construactor and Destructor */
+#include <iostream>
+using namespace std;
+class Line {
+    double length;
+public:
+    Line(); // This is a Constructor
+    ~Line(); // This is a Destructor
+    double getLength() {
+        return length;
+    }
+    void setLength(double l) {
+        length = l;
+    }
+};
+// Constructor function definition
+Line::Line() {
+    cout << "Object is being created...";
+}
+// Destructor function definition
+Line::~Line() {
+    cout << "Object is being deleted...";
+}
+int main() {
+    Line line; // Create an object of Line
+    line.setLength(10.0); // Set the length of the line
+    cout << "\nLength of line: " << line.getLength() << endl; // Output the length
+    return 0; // The destructor will be called automatically when the object goes out of scope
+}
+// Output:
+// Object is being created...
+// Length of line: 10
+// Object is being deleted...
