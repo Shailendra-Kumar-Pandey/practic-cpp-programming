cba39a319cad7cac112d8732de2798eb56a2a90e
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-25 15:42:52 +0530

    Practic c++

--- Classes And Object/Inline Class Function/Inline Class Function Example.cpp
+++ Classes And Object/Inline Class Function/Inline Class Function Example.cpp
@@ -1,1 +1,17 @@
 /* Write a Program to Class - Inline Function */
+#include<iostream>
+using namespace std;
+inline int Max(int a, int b) { //Inline function definition
+    // This function returns the maximum of two integers
+    return (a > b) ? a : b;
+}
+int main() {
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+    cout << "Maximum of " << x << " and " << y << " is: " << Max(x, y) << endl;
+    cout<<"Max(20,10): " << Max(20, 10) << endl;
+    cout<<"Max(40,30): " << Max(40, 30) << endl;
+    cout<<"Max(0,50): " << Max(0, 50) << endl;
+    return 0;
+}
