590096633978a0fd4ba32ffb96b918f32d4c110a
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-26 23:18:37 +0530

    Practic c++

--- Classes And Object/This Pointer/Example this pointer.cpp
+++ Classes And Object/This Pointer/Example this pointer.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 class Box{
     private:
-        double length;
-        double breadth;
-        double height;
+        double length;      // Length of a box
+        double breadth;     // Breadth of a box
+        double height;      // Height of a box
     public:
     //Constructor Definition and initialize
         Box(double l= 2.0, double b = 2.0, double h = 2.0 )
         {
-            cout<<"\n Constructor Call...";
+            cout<<"\n Constructor Called..."<<endl;
             length = l;
             breadth = b;
             height = h;
@@ -19,19 +19,25 @@ class Box{
         {
             return length * breadth * height;
         }
-        bool Compare(Box b2)
+        int Compare(Box b2)
         {
-            return this->Volume() > b2.Volume();
+            return this->Volume() > b2.Volume();        // Compare the volume of two boxes and alway using 'this' pointer use arrow operator
         }
 };
 int main()
 {
-    Box box1(3.0, 2.0,4.0);
-    Box box2(10.0, 4.0, 8.0);
+    Box box1(3.0, 2.0,4.0);         // Declare Box1
+    Box box2(10.0, 4.0, 8.0);       // Declare Box2
 
     if (box1.Compare(box2))
-        cout<<"\nBox1 is Big ";
-    else
-        cout<<"\n Box2 is Big";
+    {
+        cout<<"\nBox-2 is Smaller than Box-1..."<<endl;
+    }else{
+            cout<<"\nBox-2 is equal to or larger than Box-1..."<<endl;
+    }
     return 0;
 }
+// Output:
+// Constructor Call...
+// Constructor Call...
+// Box-2 is Smaller than Box-1...
