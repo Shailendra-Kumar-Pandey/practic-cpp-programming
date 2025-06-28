43c81c5aa10fcbdad22538ec5c24866a94fd9e9e
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-28 15:10:09 +0530

    Practic c++

--- Classes And Object/Inheritance/Multiple Inheritance/Examle Multiple Inheritance.cpp
+++ Classes And Object/Inheritance/Multiple Inheritance/Examle Multiple Inheritance.cpp
@@ -1,1 +1,43 @@
 /* Write a Program to Class - Multiple Inheritance */
+#include <iostream>
+using namespace std;
+// Base class 1
+class Shape {
+    protected:
+        double width;
+        double height;
+    public:
+        void setWidth(double w) {
+            width = w;
+        }
+        void setHeight(double h) {
+            height = h;
+        }
+    };
+ // Base class 2
+ class Cost{
+    public:
+        double getCost(double area) {
+            return area * 70;
+        }   
+ };
+// Derived class
+class Rectangle : public Shape, public Cost {
+    public:
+        double getArea() {
+            return (width * height);
+        }
+};
+int main() {
+    Rectangle rect;
+    rect.setWidth(5.0);
+    rect.setHeight(3.0);
+    
+    double area = rect.getArea();
+    double cost = rect.getCost(area);
+    
+    cout << "Area of Rectangle: " << area << endl;
+    cout << "Cost of Rectangle: " << cost << endl;
+    
+    return 0;
+}
