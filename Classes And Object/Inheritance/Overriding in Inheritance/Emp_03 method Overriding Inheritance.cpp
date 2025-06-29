1fe40536deee62a658cb4640cc1669e3653902aa
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-29 15:16:11 +0530

    Practic C++

--- Classes And Object/Inheritance/Overriding in Inheritance/Emp_03 method Overriding Inheritance.cpp
+++ Classes And Object/Inheritance/Overriding in Inheritance/Emp_03 method Overriding Inheritance.cpp
@@ -1,1 +1,25 @@
 /* Write a Program to class Overriding Inheritance */
+#include <iostream>
+using namespace std;
+class Base {
+public:
+    void print() {
+        cout << "Base Function" << endl;
+    }
+};
+class Derived : public Base {
+public:
+    void print() {
+        cout << "Derived Function" << endl;
+    }
+};
+int main(){
+    Derived derived1;
+    derived1.print(); // Calls the Derived class print function
+    
+    //poiner of Base type that points to derived1
+    Base*ptr = &derived1;
+    // call Function of Base class using ptr
+    ptr->print(); // Calls the Base class print function
+    return 0;
+}
