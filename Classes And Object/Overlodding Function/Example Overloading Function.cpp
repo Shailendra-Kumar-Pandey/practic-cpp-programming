9666d62a2a45f7b2c545366a63bf50413973b643
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-30 20:16:26 +0530

    Practic c++

--- Classes And Object/Overlodding Function/Example Overloading Function.cpp
+++ Classes And Object/Overlodding Function/Example Overloading Function.cpp
@@ -1,1 +1,24 @@
 /* Write a program to class - Overloadding Function */
+#include<iostream>
+using namespace std;
+class PrintData         // Create class and member function , initilize and Defination
+{
+    public:
+        void print(int i){
+            cout<<"\n Printing Int... "<<i<<endl;
+        }
+        void print(double f){
+            cout<<"\n Printing Float... "<<f<<endl;
+        }
+        void print(char *c){
+            cout<<"\n Printing Charecter... "<<c<<endl;
+        }
+};
+int main()
+{
+    PrintData pd;       // Create Object
+    pd.print("Shailendra Kumar Pandey");        // Calling function
+    pd.print(12);
+    pd.print(9.01);
+    return 0;
+}
