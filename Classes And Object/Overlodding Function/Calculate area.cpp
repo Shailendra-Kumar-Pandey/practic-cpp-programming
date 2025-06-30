17b0cb5bc8cb27f608d8aee00932d48a9c13b0eb
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-30 23:47:02 +0530

    Practic c++

---
+++ Classes And Object/Overlodding Function/Calculate area.cpp
@@ -0,0 +1,10 @@
+/* Write a menu driven program to calculate and print areas as follows. Consider overloading function as area().
+
+To Calculate Area Menu
+1) Area of Circle
+2) Area of Rectangle
+3) Area of Triangle
+4) Area of Square
+5) Exit
+Enter your choice (1-5): 
+*/
