9e7f06cdf99219ea6c614b43c9e3cfaea05b45a6
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-30 21:02:00 +0530

    Practic c+=

--- Classes And Object/Overlodding Function/Simple Interest Overloading Function.cpp
+++ Classes And Object/Overlodding Function/Simple Interest Overloading Function.cpp
@@ -1,1 +1,22 @@
 /* Write a Program to Class and Overloading function in Simple Interest */
+#include<iostream>
+using namespace std;
+class SimpleInterest
+{
+    int rate = 5;
+    public:
+        double Simple_Interest(int p, int t, int r){
+            return (p*t*r)/100;
+        }
+        double Simple_Interest(int p, int t){
+            return (p*t*rate)/100;
+        } 
+};
+int main()
+{
+    SimpleInterest si;
+    
+    si.Simple_Interest(5000,12,7);
+    si.Simple_Interest(10000,5);
+    return 0;
+}
