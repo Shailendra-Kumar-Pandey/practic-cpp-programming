9cd5565c17db307b2fd7e3bb43135f52b1d6b8f3
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-07-08 23:15:33 +0530

    practic c++

--- Classes And Object/Polymorphism in C++/Example in Polymorphism.cpp
+++ Classes And Object/Polymorphism in C++/Example in Polymorphism.cpp
@@ -1,1 +1,61 @@
 /* Write a Program to Class - Polymorphism in c++ */
+#include <iostream>
+using namespace std;
+class Shape         // Base Class 
+{
+    protected:
+        int width;
+        int height;
+    public:
+        Shape(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        int area()
+        {
+            cout << "\nPerent Class Area " << endl;
+            return 0;
+        }      
+};
+class Rectangle : public Shape  // Derived Class
+{
+    public:
+        Rectangle(int a = 0, int b = 0) 
+        {
+             Shape(a, b);  // Call base class constructor
+        }
+        int area()  // Override area method
+        {
+            cout << "\nRectangle Class Area " << endl;
+            return width * height;  // Calculate area of rectangle
+        }
+};
+class Triangle : public Shape  // Derived Class
+{
+    public:
+        Triangle(int a = 0, int b = 0) 
+        {
+             Shape(a, b);  // Call base class constructor
+        }
+        int area()  // Override area method
+        {
+            cout << "\nTriangle Class Area " << endl;
+            return (width * height / 2);  // Calculate area of rectangle
+        }
+};
+int main()
+{
+    Shape *shape;  // Base class pointer
+    Rectangle rec(10, 7);  // Create Rectangle object
+    Triangle tri(10, 5);  // Create Triangle object
+
+    shape = &rec;  // Point to Rectangle object
+    cout << "Area of Rectangle: " << shape->area() << endl;  // Call area method
+
+    shape = &tri;  // Point to Triangle object
+    cout << "Area of Triangle: " << shape->area() << endl;  // Call area method
+
+    return 0;
+}
+
