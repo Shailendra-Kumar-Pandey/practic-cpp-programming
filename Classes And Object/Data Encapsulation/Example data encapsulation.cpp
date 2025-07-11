54e6445f34c1d0be880e0ac96b508f169f4c9aef
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-07-11 21:45:56 +0530

    Practic c++

--- Classes And Object/Data Encapsulation/Example data encapsulation.cpp
+++ Classes And Object/Data Encapsulation/Example data encapsulation.cpp
@@ -1,1 +1,35 @@
 /* Write a Program to Class - Data Encapsulation */
+#include<iostream>
+using namespace std;
+class Adder     // Base Class
+{
+    public:
+        // Constructor
+        Adder(int i =0)
+        {
+            total = i;  // Initialize total
+        }
+        // interface to outside world
+        void addNum(int number)
+        {
+            total += number;
+        }
+        // interface to outside world
+        int getTotal()
+        {
+            return total;
+        }
+    private:
+        // hidden data from outside world
+        int total;
+};
+int main()
+{
+    Adder a;   // Create an object 
+    a.addNum(10);
+    a.addNum(20);
+    a.addNum(30);
+
+    cout << "Total: " << a.getTotal() << endl;  // Display total
+    return 0;
+}
