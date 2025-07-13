b933c0025ea6d7a3afe5b60ece2294f54ad239bf
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-07-13 16:32:48 +0530

    Practic c++

--- Classes And Object/Files and Streams/File create and write mode.cpp
+++ Classes And Object/Files and Streams/File create and write mode.cpp
@@ -8,4 +8,26 @@ int main()
     ofstream createFile;  // Create an object of ofstream class
     createFile.open("fileCreate.dat");  // Open a file in write mode
     
+    cout << "\nEnter the details..." << endl;
+    cout << "\nEnter your name: ";
+    cin.getline(data, 100);  // Read a line of text from the user
+    createFile << data << endl;  // Write the name to the file
+
+    cout << "\nEnter your age: ";
+    cin >> data;  // Read age as a string
+    cin.ignore();  // To ignore the newline character left in the buffer
+    createFile << data << endl;  // Write the age to the file
+
+    cout << "\nEnter your city: ";
+    cin.getline(data, 100);  // Read city name
+    createFile << data << endl;  // Write the city to the file
+
+    cout << "\nEnter your Moble Number: ";
+    cin >> data;  // Read mobile number as a string
+    cin.ignore();  // To ignore the newline character left in the buffer
+    createFile << data << endl;  // Write the mobile number to the file
+
+    createFile.close();  // Close the file
+    
+    return 0;
 }
