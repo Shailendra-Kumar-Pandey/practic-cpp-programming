b573601552e8e5418962f708762184c77858f979
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-07-13 16:12:59 +0530

    Practic c++

--- Classes And Object/Files and Streams/Example for file.cpp
+++ Classes And Object/Files and Streams/Example for file.cpp
@@ -6,20 +6,20 @@ int main()
 {
     char data[100];
 
-    ofstream obj;       // Create an object of ofstream class
+    ofstream obj;       // Create an object of of stream class
     obj.open("data.dat");  // Open a file in write mode
-    
+
     cout<<"\nEnter the detailes..."<<endl;
     cout<<"\nEnter your name: ";
     cin.getline(data, 100);     // Read a line of text from the user
     obj<<data<<endl;        // Write the name to the file
-    
+
     cout<<"\n Enter your age: ";
     cin>>data;      // Read age as a string
     cin.ignore();   // To ignore the newline character left in the buffer
     obj<<data<<endl;        // Write the age to the file
-    
+
     obj.close();        // Close the file
-    
+
     return 0;
 }
