19a3d822539d1338ba0770d0e016b99bb3a1947d
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-07-14 21:18:33 +0530

    Practic c++

--- Classes And Object/Try Catch & Throw/Example for try_catch_throw.cpp
+++ Classes And Object/Try Catch & Throw/Example for try_catch_throw.cpp
@@ -1,1 +1,25 @@
 /* Write a Program to using try, catch & throw keyword and in this program */
+#include <iostream>
+using namespace std;
+double division(int a, int b)
+{
+    if( b == 0)
+    {
+        throw "Division by Zero conditional";
+    }
+    return (a/b);
+}
+int main()
+{
+    int x = 50;
+    int y = 0;
+    double z = 0;
+
+    try{
+        z = division(x,y);
+        cout<<z<<endl;
+    }catch(const char *msg){
+        cerr << msg << endl;
+    }
+    return 0;
+}
