720730bad8787d64758cfa19b688e0f8547c5d6d
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-07-15 21:05:34 +0530

    practic c++

--- Classes And Object/Try Catch & Throw/Example 02 for try_catch_throw.cpp
+++ Classes And Object/Try Catch & Throw/Example 02 for try_catch_throw.cpp
@@ -1,1 +1,17 @@
-/* Write a progam to usig try and catch and throw */
+/* Write a program to using try and catch and throw */
+#include <iostream>
+#include <exception>
+using namespace std;
+double division(int a, int b)
+{
+    return (a / b);
+}
+int main()
+{
+    int x = 50;
+    int y = 0;
+    double z = 0;
+        z = division(x, y);
+        cout << z << endl;
+    return 0;
+}
