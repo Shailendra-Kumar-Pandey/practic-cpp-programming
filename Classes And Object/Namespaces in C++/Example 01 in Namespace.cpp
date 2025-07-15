f4a6205daeb4aa2b06d032a3c60c67c9490926f4
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-07-16 03:02:25 +0530

    Practic c++

--- Classes And Object/Namespaces in C++/Example 01 in Namespace.cpp
+++ Classes And Object/Namespaces in C++/Example 01 in Namespace.cpp
@@ -1,1 +1,17 @@
 /* Write a program to using namespace program */
+#include <iostream>
+using namespace std;
+namespace first
+{
+    int var = 5;
+}
+namespace second
+{
+    double var = 3.1416;
+}
+int main()
+{
+    cout << first::var << endl;
+    cout << second::var << endl;
+    return 0;
+}
