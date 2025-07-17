abffd802bb8f47f30d3654905589e3cfd7bede1e
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-07-17 21:14:12 +0530

    add assignment Question

--- Assignment in C++/Assignment in C++.cpp
+++ Assignment in C++/Assignment in C++.cpp
@@ -1,1 +1,13 @@
 /* Write a Program to Assignment in C++ */
+/*                 ABC Co. Pvt. Ltd
+                       Nagpur
+             Employee Management System
+
+                        MENU
+            1) Add Employee
+            2) Edit Employee
+            3) Delete Employee
+            4) Display Employee
+            5) Salary Register
+            6) Exit
+*/
